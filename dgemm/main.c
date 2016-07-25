6cf7d587f3b9db6cccac43eb2511d26d6c564c30
Kattya Desiderio <desideri@espol.edu.ec>
2016-07-25 00:23:13 -0500

    Implementado algoritmo dgemm

--- dgemm/main.c
+++ dgemm/main.c
@@ -14,49 +14,77 @@
  */
 
 void crear_aleatorios();
+void dgemm();
 /*void dgemm(double A[][], double B[][], double C[][]);
 */
- double B[32][32], C[32][32];
-int x;
+double B[32][32], C[32][32];
+double A[32][32];
 
 int main(int argc, char** argv) {
-  
-   
-     crear_aleatorios();
-     x=5;
-    printf("%d",x);
+    printf("********************\n");
+    printf("**ALGORITMO DGEMM***\n\n");
+    crear_aleatorios();
+    dgemm();
+    int i,j;
+    printf("********  C RESULTADO  *********\n");
+    for(i=0;i!=32;i=i+1){
+             for(j=0;j!=32;j=j+1){ 
+                // printf("%d",A[i][j]);
+                 printf("%g",C[i][j]);
+                }
+            printf("\n");
+           }
 
- 
-    return 0;
+    return (EXIT_SUCCESS);
 }
 
 void crear_aleatorios(){
      int numero= 32;
-      double A[32][32];
+     
      int num;
      int i,j;
      
     srand(time(NULL));
+    printf("********  A  *********\n");
     for(i=0;i!=32;i=i+1){
      for(j=0;j!=32;j=j+1){ 
          num = rand()%10; 
-         A[i][j]=num;
-        
+         A[i][j]=num; 
         // printf("%d",A[i][j]);
-         printf("%.2f",A[i][j]);
+         printf("%g",A[i][j]);
         }
     printf("\n");
-   }
-    
-}
-/*
-void dgemm(double A[][], double B[][], double C[][]){
-    int i, j,k;
+    }
+     printf("********  B  *********\n");
     for(i=0;i!=32;i=i+1){
-    for(j=0;j!=32;j=j+1){
-    for(k=0;k!=32;k=k+1){
-      c[i][j]=c[i][j]+a[i][k] * b[k][j];
-}}}
-}*/
+         for(j=0;j!=32;j=j+1){ 
+             num = rand()%10; 
+             B[i][j]=num; 
+            // printf("%d",A[i][j]);
+             printf("%g",B[i][j]);
+            }
+        printf("\n");
+       }
+    printf("********  C  *********\n");
+    for(i=0;i!=32;i=i+1){
+            for(j=0;j!=32;j=j+1){ 
+                num = rand()%10; 
+                C[i][j]=num; 
+               // printf("%d",A[i][j]);
+                printf("%g",C[i][j]);
+               }
+           printf("\n");
+          }
+    }
+
+
+void dgemm(){
+    int i, j,k;
+        for(i=0;i!=32;i=i+1){
+        for(j=0;j!=32;j=j+1){
+        for(k=0;k!=32;k=k+1){
+          C[i][j]=C[i][j]+A[i][k] * B[k][j];
+        }}}
+}
 
 
